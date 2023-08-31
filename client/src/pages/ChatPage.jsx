1e91172c6c2710d2c342b4d881323f08fa7f6382
FarhadIbrahimov <farhadibrahimov@gmail.com>
2023-08-31 03:45:00 -0600

    rendered data to the page

--- client/src/pages/ChatPage.jsx
+++ client/src/pages/ChatPage.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const ChatPage = () => {
-  return <div>ChatPage</div>;
+  const [chats, setChats] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get("/api/chat");
+        const { data } = response;
+        setChats(data);
+        console.log(data);
+      } catch (error) {
+        console.log();
+        console.log("Error fetching chats: ", error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <ul>
+        {chats.map((chat) => (
+          <li key={chat._id}>{chat.chatName}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default ChatPage;
