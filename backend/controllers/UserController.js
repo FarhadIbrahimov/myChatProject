a5981ba5b0f45df53dbb83f90538aa3718aca858
FarhadIbrahimov <farhadibrahimov@gmail.com>
2023-08-31 13:14:57 -0600

    completed user Authentication and Error Handling

---
+++ backend/controllers/UserController.js
@@ -0,0 +1,77 @@
+// Import the necessary modules and dependencies
+const asyncHandler = require("express-async-handler");
+const User = require("../models/UserModel"); // Import the User model
+const generateToken = require("../config/generateToken"); // Import a function to generate tokens
+
+// Define a function that handles user registration
+const registerUser = asyncHandler(async (req, res) => {
+  // Extract necessary information from the request body
+  const { name, email, password, pic } = req.body;
+
+  // Check if required fields are missing
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please Enter all the fields"); // Throw an error if fields are missing
+  }
+
+  // Check if a user with the same email already exists
+  const userExist = await User.findOne({ email });
+  if (userExist) {
+    res.status(400);
+    throw new Error("User already exists"); // Throw an error if user already exists
+  }
+
+  // Create a new user record in the database
+  const user = await User.create({
+    name,
+    email,
+    password,
+    pic,
+  });
+
+  // Check if user creation was successful
+  if (user) {
+    // If successful, send a response with user details and a token
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      pic: user.pic,
+      token: generateToken(user._id), // Generate and include a token for authentication
+    });
+  } else {
+    res.status(400);
+    throw new Error("Failed to Create the user"); // Throw an error if user creation fails
+  }
+});
+
+// Define a function to handle user authentication
+const authUser = asyncHandler(async (req, res) => {
+  // Extract email and password from the form fields in the request
+  const { email, password } = req.body;
+
+  // Find a user in the database using the provided email
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    // If the user exists, respond with user details and a token
+    res.status(201); // Set the response status code to "Created"
+    res.json({
+      _id: user.id, // User's unique ID
+      name: user.name,
+      email: user.email,
+      pic: user.pic,
+      token: generateToken(user._id), // Generate and include a token for authentication
+    });
+  } else {
+    // If the user doesn't exist or the password is incorrect
+    res.status(401); // Set the response status code to "Unauthorized"
+    throw new Error("Invalid ID or Password"); // Show an error message
+  }
+});
+
+// Export the authUser function for use in other parts of the application
+module.exports = authUser;
+
+// Export the registerUser function for use in other parts of the application
+module.exports = { registerUser, authUser };
