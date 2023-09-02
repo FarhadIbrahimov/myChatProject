4c62fcf4ddd859970a9c2f41c713cd61579a8f91
FarhadIbrahimov <farhadibrahimov@gmail.com>
2023-09-01 23:28:41 -0600

    created Add To and Remove From Chat Group fucntions, tested in POSTMAN ok!

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -4,14 +4,17 @@ const {
   accessChat,
   fetchChats,
   createGroupChat,
+  renameGroup,
+  addToGroup,
+  removeFromGroup,
 } = require("../controllers/chatControllers");
 const { protect } = require("../middleware/authMiddleware");
 
 router.route("/").post(protect, accessChat);
 router.route("/").get(protect, fetchChats);
 router.route("/group").post(protect, createGroupChat);
-// router.route("/rename").put(protect, renameGroup);
-// router.route("/groupRemove").put(protect, removeGroup);
-// router.route("/groupAdd").put(protect, addGroup);
+router.route("/rename").put(protect, renameGroup);
+router.route("/groupAdd").put(protect, addToGroup);
+router.route("/groupRemove").put(protect, removeFromGroup);
 
 module.exports = router;
