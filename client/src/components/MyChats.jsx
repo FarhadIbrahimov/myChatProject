6c1764d573a8d2bd9ab16d7341cfb14f3664e9c6
FarhadIbrahimov <farhadibrahimov@gmail.com>
2023-09-06 03:20:55 -0600

    created MyChat functionality

--- client/src/components/MyChats.jsx
+++ client/src/components/MyChats.jsx
@@ -1,8 +1,10 @@
 import { ChatState } from "../Context/ChatProvider";
 import { useEffect, useState } from "react";
-import { useToast, Box, Button } from "@chakra-ui/react";
+import { useToast, Box, Button, Stack, Text } from "@chakra-ui/react";
 import axios from "axios";
 import { AddIcon } from "@chakra-ui/icons";
+import ChatLoading from "./ChatLoading";
+import { getSender } from "../config/ChatLogics";
 
 const MyChats = () => {
   const [loggedUser, setLoggedUser] = useState();
@@ -39,7 +41,8 @@ const MyChats = () => {
 
   return (
     <Box
-      display={{ base: selectedChat ? "none" : "flex", md: "flex" }}
+      // display={{ base: selectedChat ? "none" : "flex", md: "flex" }}
+      display={{ base: "flex", md: "flex" }}
       flexDir="column"
       alignItems="center"
       p={3}
@@ -51,21 +54,64 @@ const MyChats = () => {
       <Box
         pb={3}
         px={3}
-        fontSize={{ base: "28px", md: "30px" }}
+        fontSize={{ base: "20px", md: "30px" }}
         fontFamily="Work sans"
         display="flex"
+        flexDir={{ base: "column", md: "row" }} // Stack vertically on small screens, align horizontally on medium and larger screens
+        alignItems="center"
         w="100%"
         justifyContent="space-between"
-        alignItems="center"
       >
-        My Chats
+        <Text mb={{ base: 2, md: 0 }}>My Chats</Text>{" "}
+        {/* mb for margin-bottom */}
         <Button
-          display="flex"
-          fontSize={{ base: "17px", md: "10px", lg: "17px" }}
+          fontSize={{ base: "14px", md: "20px" }}
           rightIcon={<AddIcon />}
+          mt={{ base: 2, md: 0 }}
+          width={{ base: "100%", md: "auto" }} // Allow button to take full width on small screens, auto width on larger screens
+          maxW="80%"
+          overflow="hidden"
+          whiteSpace="nowrap"
+          textOverflow={"ellipsis"}
         >
           New Group Chat
         </Button>
+      </Box>
+
+      <Box
+        display="flex"
+        flexDir="column"
+        p={3}
+        bg="#F8F8F8"
+        w="100%"
+        h="100%"
+        borderRadius="lg"
+        overflowY="hidden"
+      >
+        {chats ? (
+          <Stack overflowY="scroll">
+            {chats.map((chat) => (
+              <Box
+                onClick={() => setSelectedChat(chat)}
+                cursor="pointer"
+                bg={selectedChat === chat ? "#38B2AC" : "#E8E8E8"}
+                color={selectedChat === chat ? "white" : "black"}
+                px={3}
+                py={2}
+                borderRadius={chat._id}
+                key={chat._id}
+              >
+                <Text>
+                  {!chat.isGroupChat
+                    ? getSender(loggedUser, chat.users)
+                    : chat.chatName}
+                </Text>
+              </Box>
+            ))}
+          </Stack>
+        ) : (
+          <ChatLoading />
+        )}
       </Box>
     </Box>
   );
