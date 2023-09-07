9a522c98fe883440f6b8014fc937e1c44567f3cd
FarhadIbrahimov <farhadibrahimov@gmail.com>
2023-09-07 12:36:27 -0600

    removed colors from backend

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const { chats } = require("./data/data");
 const connectDB = require("./config/db");
-const colors = require("colors");
+// const colors = require("colors");
 const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
@@ -28,4 +28,4 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server Started on PORT ${PORT}`.blue.bold));
+app.listen(PORT, console.log(`Server Started on PORT ${PORT}`));
