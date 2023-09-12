d7bd02fbc55610346aa0d515d2c7d312ffe19b84
FarhadIbrahimov <farhadibrahimov@gmail.com>
2023-09-11 23:29:38 -0600

    npm i socket.io in the root folder

--- backend/server.js
+++ backend/server.js
@@ -28,4 +28,6 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server Started on PORT ${PORT}`));
+const server = app.listen(PORT, console.log(`Server Started on PORT ${PORT}`));
+
+const io = require("socket.io")(server, {});
